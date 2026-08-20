b7ab7a73d928bfdee8febae20bf2c11bbba23661
Saúl Marrero <smb.0709pm@proton.me>
2026-08-20 15:14:56 +0100

    Update
    Dialogos mejorados

--- scripts/paintings.gd
+++ scripts/paintings.gd
@@ -1,8 +1,9 @@
 extends Area2D
-
 @onready var cl = $"../player/canvaslayer"
-@onready var labelName = $"../player/canvaslayer/characterSprite/labelName"
+@onready var marco = $"../player/canvaslayer/Marco"
+@onready var characterSprite = $"../player/canvaslayer/characterSprite"
 @onready var labelText = $"../player/canvaslayer/characterSprite/labelText"
+@onready var labelName = $"../player/canvaslayer/characterSprite/labelName"
 
 var textos: Array[String] = [
 	"Hola soy la oveja",
@@ -10,22 +11,74 @@ var textos: Array[String] = [
 	"Ten cuidado con el lobo"
 ]
 
-var dialog_index := 0
+var index := 0
 var active := false
+var typing := false
+var skip_typing := false
+
+var marco_pos: Vector2
+var sprite_pos: Vector2
+var offset := Vector2(0, 1000)
+var slide_time := 0.5
+
+func _ready() -> void:
+	marco_pos = marco.position
+	sprite_pos = characterSprite.position
+	marco.position = marco_pos + offset
+	characterSprite.position = sprite_pos + offset
 
 func _on_input_event(viewport, event, shape_idx) -> void:
 	if event.is_action_pressed("click") and not active:
 		active = true
-		cl.visible = true
+		index = 0
 		labelName.text = "Miss\nSheep"
-		labelText.text = textos[dialog_index]
+		_open_dialog()
+		get_viewport().set_input_as_handled()
 
-func _input(event: InputEvent) -> void:
-	if active and event.is_action_pressed("click"):
-		dialog_index += 1
-		if dialog_index < textos.size():
-			labelText.text = textos[dialog_index]
+func _unhandled_input(event: InputEvent) -> void:
+	if not active or not event.is_action_pressed("click"):
+		return
+	if typing:
+		skip_typing = true
+	else:
+		index += 1
+		if index < textos.size():
+			_type_line(textos[index])
 		else:
-			cl.visible = false
-			active = false
-			dialog_index = 0
+			_close_dialog()
+
+func _open_dialog() -> void:
+	cl.visible = true
+	labelText.text = ""
+	var tween := create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(marco, "position", marco_pos, slide_time)\
+		.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+	tween.tween_property(characterSprite, "position", sprite_pos, slide_time)\
+		.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+	tween.chain().tween_callback(func(): _type_line(textos[index]))
+
+func _type_line(text: String) -> void:
+	typing = true
+	skip_typing = false
+	labelText.text = ""
+	for c in text:
+		if skip_typing:
+			break
+		labelText.text += c
+		await get_tree().create_timer(0.03).timeout
+	labelText.text = text
+	typing = false
+
+func _close_dialog() -> void:
+	var tween := create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(marco, "position", marco_pos + offset, slide_time * 0.6)\
+		.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN)
+	tween.tween_property(characterSprite, "position", sprite_pos + offset, slide_time * 0.6)\
+		.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN)
+	tween.chain().tween_callback(func():
+		cl.visible = false
+		active = false
+		index = 0
+	)
